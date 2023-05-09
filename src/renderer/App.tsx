12a9ac58e7397407d0a16aa15334f2f4f1e76c88
Yajima Soichi <bildeneins@gmail.com>
2023-05-09 13:40:12 +0900

    Update design

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,7 +1,7 @@
 import './App.css';
 
 export default function App() {
-  const ls = [...Array(24)].map((_, i) => i);
+  const ls = [...Array(25)].map((_, i) => i);
   return (
     <div className="grid-container">
       {ls.map((i) => (
@@ -9,6 +9,19 @@ export default function App() {
           <div className="grid-label">{i}</div>
         </div>
       ))}
+      <div className="event-container">
+        <div
+          style={{
+            borderRadius: '8px',
+            height: 'calc(100% / 25)',
+            background: 'rgba(125, 125, 125, 0.3)',
+            color: 'gray',
+            fontWeight: 'bold',
+          }}
+        >
+          0:00 test
+        </div>
+      </div>
     </div>
   );
 }
